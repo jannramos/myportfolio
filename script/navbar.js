573dc060e2ce503036744530dbd25d3afa0561fd
jannramos <jhonalynanndino009@gmail.com>
2025-08-05 13:12:23 +0800

    commit

--- script/navbar.js
+++ script/navbar.js
@@ -1,6 +1,13 @@
 const burger = document.getElementById('burger');
-  const navLinks = document.getElementById('navLinks');
+  const sideMenu = document.getElementById('sideMenu');
+  const closeBtn = document.getElementById('closeBtn');
 
   burger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
+    sideMenu.classList.add('open');
+    document.body.style.overflow = 'hidden'; // Prevent scrolling
+  });
+
+  closeBtn.addEventListener('click', () => {
+    sideMenu.classList.remove('open');
+    document.body.style.overflow = 'auto';
   });
