166823727dd08bb5e09842421229f6e67bc18c28
jannramos <jhonalynanndino009@gmail.com>
2025-08-05 13:16:56 +0800

    commit

--- script/navbar.js
+++ script/navbar.js
@@ -1,10 +1,10 @@
-const burger = document.getElementById('burger');
+ const burger = document.getElementById('burger');
   const sideMenu = document.getElementById('sideMenu');
   const closeBtn = document.getElementById('closeBtn');
 
   burger.addEventListener('click', () => {
     sideMenu.classList.add('open');
-    document.body.style.overflow = 'hidden'; // Prevent scrolling
+    document.body.style.overflow = 'hidden';
   });
 
   closeBtn.addEventListener('click', () => {
